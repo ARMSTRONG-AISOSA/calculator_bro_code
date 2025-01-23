be91b9c56c26f8ebd9db6252aa5f920f7ad52987
Armstrong Omoregie <ammycan6@gmail.com>
2025-01-23 16:06:08 +0100

    Complete

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-// / CALCULATOR PROGRAM
+// Input Display
+const display = document.getElementById("display");
 
-// const display = document.getElementById("display");
+// This function is used to dynamically build the expression on the display as the user clicks the buttons
+function appendToDisplay(input) {
+    // adds the new value to the current/present stored 
+    display.value += input;
+    // display.value = display.value + input;
+}
 
-// function appendToDisplay(input){
-//     display.value += input;
-// }
+function clearDisplay(input) {
+    display.value = "";
+}
 
-// function clearDisplay(){
-//     display.value = "";
-// }
-
-// function calculate(){
-//     try{
-//         display.value = eval(display.value);
-//     }
-//     catch(error){
-//         display.value = "Error";
-//     }
-// }
+function calculate() {
+    // Evaluate/Calculate what's within the display and update the display.value 
+    try {
+        // Use a library with react
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
